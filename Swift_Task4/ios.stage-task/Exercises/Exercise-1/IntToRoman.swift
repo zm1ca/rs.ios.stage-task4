6bf71390faa9dcc37d9307efb4f385ca70b35489
zm1ca <zmicier.fied@yandex.ru>
2021-06-03 20:29:26 +0300

    feat: add code for exercise 1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,20 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        guard self <= 3999 && self >= 1 else { return nil }
+        
+        let coding: [Int:String] = [1: "I", 4: "IV", 5:"V", 9:"IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100:"C", 400:"CD", 500: "D", 900: "CM", 1000:"M"]
+        
+        var result = ""
+        var number = self
+        
+        for key in coding.keys.sorted(by: >) {
+            while number >= key {
+                number -= key
+                result.append(coding[key]!)
+            }
+        }
+        
+        return result
     }
 }
